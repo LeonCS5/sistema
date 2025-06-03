e535149935b4f859e7d6a1161774c333fd314146
Matheus <99998949+MatheusCoss@users.noreply.github.com>
2025-06-03 10:41:19 -0300

    css foda

--- public/index.php
+++ public/index.php
@@ -12,22 +12,33 @@ $result = $conn->query($sql);
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Loja Online</title>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="../css/index.css">
+
 </head>
 <body>
     <header class="headerClass">
-        <div><img src="" alt=""></div>
+        <div class="icone-container">
+            <img src="../images/icone_galopes.svg" alt="" class="icone">
+            <p>Lopes</p>
+        </div>
         <div class="login-buttons">
             <a href="login.php" class="btn">Login</a>
             <a href="cadastro.php" class="btn">Cadastrar</a>
         </div>
     </header>
+    
+    <div>
+        <img src="../images/agua2.jpg" alt="">
+    </div>
 
 
 
     <div class="container">
         
-        <h1>Produtos Disponíveis</h1>
+        
         <div class="product-list">
             <?php while($produto = $result->fetch_assoc()): ?>
             <div class="product-card">
